425f6c0be2a418e74ca234bf7f097f0eb3ec7322
Xin Chen <xinszchen@thoughtworks.com>
2018-09-04 23:26:32 +0800

    feat: practice 6

--- src/practice_6/teacher.js
+++ src/practice_6/teacher.js
@@ -1,1 +1,17 @@
 
+import Person from './person';
+
+export default class Teacher extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klasses = klass || null;
+    }
+
+    classInformation() {
+        return this.klasses ? `Class ${this.klasses}` : 'No Class';
+    }
+
+    introduce() {
+        return `${super.introduce()} I am a Teacher. I teach ${this.classInformation()}.`;
+    }
+}
