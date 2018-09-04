6dc5603e4231b69d1245ae9a9fc87dd6ce742eb6
Xin Chen <xinszchen@thoughtworks.com>
2018-09-04 23:26:47 +0800

    feat: practice 8

--- src/practice_8/class.js
+++ src/practice_8/class.js
@@ -1,1 +1,20 @@
+export default class Class {
+    constructor(number) {
+        this.number = number;
+        this.leader = null;
+    }
 
+    toString() {
+        return this.number;
+    }
+
+    assignLeader(student) {
+        if (this === student.klass) {
+            this.leader = student;
+        }
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+}
