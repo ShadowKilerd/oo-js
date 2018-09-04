8c687df31473f3d644f7bcd91e1aec8f3f92631e
Xin Chen <xinszchen@thoughtworks.com>
2018-09-04 23:25:19 +0800

    feat: rename test case

--- spec/practice_9/main_spec.js
+++ spec/practice_9/main_spec.js
@@ -69,7 +69,7 @@ describe("Person", () => {
             const teacher = new Teacher(1, "Tom", 21, klass);
             expect(teacher.name).to.equal("Tom");
             expect(teacher.age).to.equal(21);
-            expect(teacher.klass).to.equal(klass);
+            expect(teacher.klasses).to.equal(klass);
         });
 
         describe("#introduce", () => {
