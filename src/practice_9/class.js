808a0e0cf78693dae25138b7314bfcb5445cc6e1
Xin Chen <xinszchen@thoughtworks.com>
2018-09-04 23:27:00 +0800

    feat: practice 9

--- src/practice_9/class.js
+++ src/practice_9/class.js
@@ -1,1 +1,31 @@
+export default class Class {
+    constructor(number) {
+        this.number = number;
+        this.leader = null;
+        this.students = [];
+    }
 
+    toString() {
+        return this.number;
+    }
+
+    assignLeader(student) {
+        if (this === student.klass) {
+            this.leader = student;
+            if (!this.students.includes(student)) {
+                console.log('It is not one of us.');
+            }
+        }
+    }
+
+    appendMember(student) {
+        student.klass = this;
+        if (!this.students.includes(student)) {
+            this.students.push(student);
+        }
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+}
