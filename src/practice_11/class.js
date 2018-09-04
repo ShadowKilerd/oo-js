6ade19e0bda7600bb7dabb3074dfc7d194569c97
Xin Chen <xinszchen@thoughtworks.com>
2018-09-04 23:26:04 +0800

    feat: practice 11

--- src/practice_11/class.js
+++ src/practice_11/class.js
@@ -1,1 +1,46 @@
+export default class Class {
+    constructor(number) {
+        this.number = number;
+        this.leader = null;
+        this.students = [];
+        this.leaderListeners = [];
+        this.assignListeners = [];
+    }
 
+    toString() {
+        return this.number;
+    }
+
+    assignLeader(student) {
+        if (this === student.klass) {
+            this.leader = student;
+            this.leaderListeners.map(person => person.getNotice(student, this, true));
+        } else {
+            console.log('It is not one of us.');
+        }
+    }
+
+    appendMember(student) {
+        student.klass = this;
+        if (!this.students.includes(student)) {
+            this.students.push(student);
+            this.assignListeners.map(person => person.getNotice(student, this, false));
+        }
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+
+    isIn(student) {
+        return this === student.klass;
+    }
+
+    registerAssignLeaderListener(teacher) {
+        this.leaderListeners.push(teacher);
+    }
+
+    registerJoinListener(teacher) {
+        this.assignListeners.push(teacher);
+    }
+}
--- src/practice_11/teacher.js
+++ src/practice_11/teacher.js
@@ -1,1 +1,34 @@
+import Person from './person';
 
+export default class Teacher extends Person {
+    constructor(id, name, age, klasses) {
+        super(id, name, age);
+        this.klasses = klasses || [];
+    }
+
+    classInformation() {
+        const classesInfor = this.klasses.map(item => item.number).join(', ');
+        return this.klasses.length !== 0 ? `Class ${classesInfor}` : 'No Class';
+    }
+
+    introduceWith(student) {
+        const isTeaching = this.klasses === student.klass ? '' : 'don\'t ';
+        return `${super.introduce()} I am a Teacher. I ${isTeaching}teach ${student.name}.`;
+    }
+
+    isTeaching(student) {
+        return this.klasses.map(klass => klass.isIn(student)).includes(true);
+    }
+
+    introduce() {
+        return `${super.introduce()} I am a Teacher. I teach ${this.classInformation()}.`;
+    }
+
+    getNotice(student, klass, isLeader) {
+        if (isLeader) {
+            console.log(`I am ${this.name}. I know ${student.name} become Leader of Class ${klass.number}.`);
+        } else {
+            console.log(`I am ${this.name}. I know ${student.name} has joined Class ${klass.number}.`);
+        }
+    }
+}
