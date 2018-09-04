5590d6ab50fec64444ae34661279c2b157b6c8db
Xin Chen <xinszchen@thoughtworks.com>
2018-09-04 23:25:42 +0800

    refactor: practice 1

--- src/practice_1/person.js
+++ src/practice_1/person.js
@@ -1,11 +1,12 @@
-
-
 export default class Person {
     constructor(name, age) {
         this.name = name;
         this.age = age;
+        this.introduce = this.introduce.bind(this);
     }
+
+
     introduce() {
-        return `My name is ${this.name}. I am ${this.age} years old.`
+        return `My name is ${this.name}. I am ${this.age} years old.`;
     }
 }
