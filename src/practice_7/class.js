8ed14101c99c5de5cfa8456e1cec7be4fbc92ec0
Xin Chen <xinszchen@thoughtworks.com>
2018-09-04 23:26:40 +0800

    feat: practice 7

--- src/practice_7/class.js
+++ src/practice_7/class.js
@@ -1,1 +1,13 @@
+export default class Class {
+    constructor(number) {
+        this.number = number;
+    }
 
+    toString() {
+        return this.number;
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+}
--- src/practice_7/teacher-option2.js
+++ src/practice_7/teacher-option2.js
@@ -1,1 +1,26 @@
 
+import Person from './person';
+
+export default class Teacher extends Person {
+    constructor(name, age, klass) {
+        super(name, age);
+        this.klasses = klass || null;
+    }
+
+    classInformation() {
+        return this.klasses ? `Class ${this.klasses}` : 'No Class';
+    }
+
+    getDisplayName() {
+
+    }
+
+    introduceWith(student) {
+        const isTeaching = this.klasses === student.klass ? '' : 'don\'t ';
+        return `${super.introduce()} I am a Teacher. I ${isTeaching}teach ${student.name}.`;
+    }
+
+    introduce() {
+        return `${super.introduce()} I am a Teacher. I teach ${this.classInformation()}.`;
+    }
+}
